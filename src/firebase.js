76037680546e00178e7fdfd504ddcf2066ef580d
ManasKapoor07 <75942485+ManasKapoor07@users.noreply.github.com>
2023-09-10 12:48:27 +0530

    new

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,24 @@
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAUo_BnnXBRJ86D-c-zEDm_Q0Ts0ph2wYo",
-    authDomain: "movie-flix-a794b.firebaseapp.com",
-    projectId: "movie-flix-a794b",
-    storageBucket: "movie-flix-a794b.appspot.com",
-    messagingSenderId: "652345827024",
-    appId: "1:652345827024:web:f98f02261abb42417408d5",
-    measurementId: "G-ENL13Q6WD6"
-};
+    apiKey: "AIzaSyAlAOfwTDSb_0J9KsgBoqoEgprOPDQVN7A",
+    authDomain: "netflix-bba3f.firebaseapp.com",
+    projectId: "netflix-bba3f",
+    storageBucket: "netflix-bba3f.appspot.com",
+    messagingSenderId: "196486219085",
+    appId: "1:196486219085:web:9f396d86843e4f45486304",
+    measurementId: "G-LK0G6HR4KC"
+  };
+
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth, db };
--- src/nav.js
+++ src/nav.js
@@ -1,31 +1,32 @@
 import React from 'react'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './nav.css'
+import {useNavigate} from 'react-router-dom'
 
 function Nav() {
     //for scrolling and removing background while scroll
     const [show, setshow] = useState(false)
+    const navigate = useNavigate();
 
-    const transitionNavBar = () =>{
-        if(window.scrollY > 100)
-        {
+    const transitionNavBar = () => {
+        if (window.scrollY > 100) {
             setshow(true)
         }
-        else{
+        else {
             setshow(false)
         }
     }
 
     //code only runs when component match
     useEffect(() => {
-    window.addEventListener('scroll' , transitionNavBar)
-    //clean up function
-      return () => {
-        window.removeEventListener('scroll',transitionNavBar)
-        
-      }
+        window.addEventListener('scroll', transitionNavBar)
+        //clean up function
+        return () => {
+            window.removeEventListener('scroll', transitionNavBar)
+
+        }
     }, [])
-    
+
 
 
 
@@ -33,16 +34,24 @@ function Nav() {
     return (
         <div className={`nav ${show && 'nav-black'}`}>
 
-        <div className='nav_contain'>
-        <img className='nav_logo'
-        src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" 
-        alt=""/>
+            <div className='nav_contain'>
+                <img
+                onClick={()=>{
+                    navigate('/')
+                }}
+                className='nav_logo'
+                    src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+                    alt="" />
 
-        <img className='nav_avatar' 
-        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
-        alt=""
-        />
-        </div>
+                <img
+                onClick={()=>
+                    navigate('/profile')
+                }
+                className='nav_avatar'
+                    src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+                    alt=""
+                />
+            </div>
         </div>
     )
 }
